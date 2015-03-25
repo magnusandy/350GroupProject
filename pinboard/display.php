d8cc75b7181698718979d3d47cac4212b064a211
PhlexPlexico <PhlexPlexico@users.noreply.github.com>
2015-03-25 00:04:07 -0600

    Add header color for chrome
    Fancy things.

--- pinboard/display.php
+++ pinboard/display.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-  <meta name="theme-color" content="00ACDB">
+  <meta name="theme-color" content="#00ACDB">
 	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
 	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
 	<!--script src="js/less-1.3.3.min.js"></script-->
