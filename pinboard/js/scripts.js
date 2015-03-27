143e79c38556a31696c8315e1261ddf52df70c2b
magnusandy <magnusandy@gmail.com>
2015-03-26 22:28:25 -0600

    Revert "Revert "Added form for updating pin""
    This reverts commit bb46c54bde05ee7ad8bed5092c39ad748f1bc04d.

--- pinboard/js/scripts.js
+++ pinboard/js/scripts.js
@@ -1,4 +1,9 @@
-function hideForm(){
+function hideNewPinForm(){
 	document.getElementById("newPinForm").className = "hidden";
 	return false;
+}
+
+function showUpdateForm(){
+	hideNewPinForm();
+	document.getElementById("updatePinForm").className = "";
 }
